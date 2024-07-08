8d3ba1948838643817a2df139ccebb0fc58740ba
KimInteger <kjs20151240@gmail.com>
2024-07-08 11:01:57 +0900

    chore_김정수 : webpack.config파일 생성

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+let name = 'app'
+
+module.exports = {
+  entry : `./src/${name}.js`,
+  output : {
+    path : path.resolve(__dirname,'dist'),
+    filename : `${name}.bundle.js`
+  }
+}
