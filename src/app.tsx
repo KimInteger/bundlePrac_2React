a92f74550d8718c95d6f7165bde43e5325252c91
KimInteger <kjs20151240@gmail.com>
2024-07-08 12:46:32 +0900

    chore_김정수 : 최종의 최종

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // React는 컴포넌트를 작성할 때 대문자를 먼저쓰는 특징이 있다. 
 // 자바스크립트지만 소괄호를 쓰는 특징이 있다.
@@ -11,4 +11,5 @@ const App : React.FC = () => {
   )
 }
 
-ReactDOM.render(<App/>,document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />)
